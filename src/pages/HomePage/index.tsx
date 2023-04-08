7d90b26eae809d74d1fc98f4461cf01ada00f2fa
duclv4 <duclv@tokyotechlab.com>
2023-04-08 08:56:16 +0700

    two way binding upload image

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -2,10 +2,16 @@ import UploadAvatar from 'components/base/UpLoad/UploadAvatar';
 import React from 'react';
 
 const HomePage: React.FC = () => {
+  const handleUploadSuccess = (id: number, url: string) => {
+    console.log('imageId', id);
+    console.log('imageSrc', url);
+    // save data to profile user here
+  };
+
   return (
     <div>
       <p>Homepage</p>
-      <UploadAvatar />
+      <UploadAvatar onSuccess={handleUploadSuccess} />
     </div>
   );
 };
